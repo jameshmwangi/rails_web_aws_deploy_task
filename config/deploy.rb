d2da2f17a2bfe7339c656df9b8400f3a1d217e8b
James Mwangi K <jameshmwangiy@gmail.com>
2026-03-04 23:06:19 +0300

    add s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock "~> 3.20.0"
 
 set :application, "cdp_web_web_aws_deploy_task"
 set :repo_url, "https://github.com/jameshmwangi/rails_web_aws_deploy_task.git"
-set :linked_files, %w{config/secrets.yml}
+set :linked_files, %w{config/secrets.yml .env}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 set :keep_releases, 5
 set :rbenv_ruby, '3.3.0'
